fca6a1aee6117d91203a0cdb8153b9e66c2f37f9
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-06-03 00:27:47 +0600

    transition added

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -14,11 +14,11 @@ const SearchBar = () => {
             placeholder="Search for city..."
             className="  bg-[#0b3631]/30 placeholder:text-slate-200 text-slate-200 p-2 rounded-md w-full focus:ring-1 ring-slate-300 outline-none"
           />
-          <CiSearch size={25} fill="white" className=" cursor-pointer hover:scale-110 transition-all ease-out " />
-          <BiCurrentLocation size={25} fill="white" className=" cursor-pointer hover:scale-110 transition-all ease-out " />
+          <CiSearch size={25} fill="white" className=" cursor-pointer hover:scale-125 transition-all ease-in  " />
+          <BiCurrentLocation size={25} fill="white" className=" cursor-pointer hover:scale-125 transition-all ease-in  " />
           <div className=" flex text-white">
-            <p onClick={()=>setToggleDegree('c')} className={`cursor-pointer px-3 py-1 rounded-l-md ${toggleDegree == "c"? "bg-[#0b3631]/50":"bg-slate-400/30" }`}>°C</p>
-            <p onClick={()=>setToggleDegree('f')} className={`cursor-pointer px-3 py-1 rounded-r-md ${toggleDegree == "f"? "bg-[#0b3631]/50":"bg-slate-400/30" }`}>°F</p>
+            <p onClick={()=>setToggleDegree('c')} className={`cursor-pointer px-3 py-1 rounded-l-md ${toggleDegree == "c"? "bg-[#0b3631]/50":"bg-slate-400/30" } transition-all ease-in duration-500`}>°C</p>
+            <p onClick={()=>setToggleDegree('f')} className={`cursor-pointer px-3 py-1 rounded-r-md ${toggleDegree == "f"? "bg-[#0b3631]/50":"bg-slate-400/30" } transition-all ease-in duration-500`}>°F</p>
           </div>
         </div>
       </div>
